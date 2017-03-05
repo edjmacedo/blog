a81c94295d34a38ecf74cffc693c40d7ec354002
Edvan Macedo <edvan.macedo.jr@gmail.com>
2017-03-05 13:04:14 -0400

    Update blog - Implement login screen

--- blog.py
+++ blog.py
@@ -253,6 +253,22 @@ class Unit3Welcome(BlogHandler):
         else:
             self.redirect('/blog/signup')
 
+class Login(BlogHandler):
+    def get(self):
+        self.render('login-form.html')
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+
+        u = User.login(username, password)
+        if u:
+            self.login(u)
+            self.redirect('/blog/welcome')
+        else:
+            msg = 'Invalid login'
+            self.render('login-form.html', error = msg)
+
 
 app = webapp2.WSGIApplication([('/', MainPage),                               
                                ('/blog/?', BlogFront),
@@ -260,5 +276,6 @@ app = webapp2.WSGIApplication([('/', MainPage),
                                ('/blog/newpost', NewPost),
                                ('/blog/signup', Register),
                                ('/blog/welcome', Unit3Welcome),
+                               ('/blog/login', Login)
                                ],
                               debug=True)
