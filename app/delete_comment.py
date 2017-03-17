af1dafbe2fb9c8a7463146fde2d090895e4f84c8
Edvan Macedo <edvan.junior@itriad.org.br>
2017-03-17 06:39:28 -0400

    Fix bug in delete

--- app/delete_comment.py
+++ app/delete_comment.py
@@ -11,8 +11,8 @@ class DeleteComment(BlogHandler):
             ## Getting comment key using comment_id
             key = db.Key.from_path('Comment', int(comment_id),
                                    parent=blog_key())
-            if c.author == int(self.read_secure_cookie('user_id')):
-                c = db.get(key)            
+            c = db.get(key)     
+            if c.author == int(self.read_secure_cookie('user_id')):                      
                 c.delete()
                 self.redirect("/"+post_id)
             else:
